4f5b1501b1b4bfb3e88282dcfeacbb1bba74fd08
Michael <Scheidl>
2020-12-02 16:28:04 +0100

    Fertig

---
+++ SpecFlowFeature1.feature
@@ -0,0 +1,29 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are added
+	Then the result should be 120
+
+Scenario: Substract two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are substracted
+	Then the result should be -20
+
+Scenario: Multiply two numbers
+	Given the first number is 50
+	And the second number is 70
+	When the two numbers are multiplied
+	Then the result should be 3500
+
+Scenario: Divide two numbers
+	Given the first number is 2
+	And the second number is 1
+	When the two numbers are divided
+	Then the result should be 2
---
+++ SpecFlowFeature1Steps.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace RQE_Pruefund2
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+
+        int number1;
+        int number2;
+        int result;
+
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            number1 = p0;
+        }
+        
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            number2 = p0;
+        }
+        
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            result = number1 + number2;
+        }
+        
+        [When(@"the two numbers are substracted")]
+        public void WhenTheTwoNumbersAreSubstracted()
+        {
+            result = number1 - number2;
+        }
+        
+        [When(@"the two numbers are multiplied")]
+        public void WhenTheTwoNumbersAreMultiplied()
+        {
+            result = number1 * number2;
+        }
+        
+        [When(@"the two numbers are divided")]
+        public void WhenTheTwoNumbersAreDivided()
+        {
+            result = number1 / number2;
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            Assert.Equal(p0, (int) result);
+        }
+       
+    }
+}
